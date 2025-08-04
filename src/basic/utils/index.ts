4b53b74899c40c812c0157eaf6aa5a7c55b67445
이민재 <odin@snaps.com>
2025-08-04 23:17:01 +0900

    feat: 순수 함수들을 utils로 분리 1. 가격 계산 2. 포맷팅 3. 검증

---
+++ src/basic/utils/index.ts
@@ -0,0 +1,12 @@
+// Calculation utilities
+export {
+  calculateProductDiscount,
+  applyBulkDiscount,
+  calculateItemPrice,
+} from "./calculations";
+
+// Formatting utilities
+export { formatUserPrice, formatAdminPrice, SOLD_OUT_TEXT } from "./formatters";
+
+// Validation utilities
+export { hasBulkPurchase, calculateRemainingStock } from "./validators";
