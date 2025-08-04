f6d342dd50af8beb5be8228f23edf2bfebf006ce
이민재 <odin@snaps.com>
2025-08-05 00:35:32 +0900

    feat: 기능별 중간 레벨 컴포넌트 분리 - ProductList 컴포넌트

--- src/basic/components/features/index.ts
+++ src/basic/components/features/index.ts
@@ -1,8 +1,8 @@
 // Feature Components (기능별 중간 레벨 컴포넌트들)
 
 export { Header } from "./Header";
+export { ProductList } from "./ProductList";
 
 // 곧 생성 예정:
-// export { ProductList } from './ProductList';
 // export { CartSidebar } from './Cart';
 // export { AdminPanel } from './Admin';
