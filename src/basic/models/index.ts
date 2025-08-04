daeca1e46411cf1d358301e28c7da83393d743d9
odin <odin@snaps.com>
2025-08-04 22:58:28 +0900

    feat: 도메인별로 타입 분리 (Models 분리) 1. cart 2. coupon 3. notification 4. product

---
+++ src/basic/models/index.ts
@@ -0,0 +1,11 @@
+// Product domain
+export type { Product, ProductWithUI, Discount } from "./product";
+
+// Cart domain
+export type { CartItem } from "./cart";
+
+// Coupon domain
+export type { Coupon } from "./coupon";
+
+// Notification domain
+export type { Notification } from "./notification";
