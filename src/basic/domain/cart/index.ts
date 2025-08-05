d43ff664fbc599188afb4b5afc6fe7d0ecf540a9
odin <odin@snaps.com>
2025-08-06 03:28:39 +0900

    feat: 불필요한 파일 제거 및 index 파일 재구성

---
+++ src/basic/domain/cart/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Cart Domain - 장바구니 관련 모든 코드
+ */
+
+// Models
+export * from "./models";
+
+// Hooks
+export * from "./hooks";
+
+// Utils
+export * from "./utils";
+
+// Components
+export * from "./components";
---
+++ src/basic/domain/notification/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Notification Domain - 알림 관련 모든 코드
+ */
+
+// Models
+export * from "./models";
+
+// Hooks
+export * from "./hooks";
+
+// Components
+export * from "./components";
