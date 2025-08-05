4dc9e2e9bd58d8248dcdf621dac359c937dcc288
이민재 <odin@snaps.com>
2025-08-06 02:41:02 +0900

    feat: 엔티티와 순수 함수 분리를 기준으로 새로운 폴더 계층 구조 변경 1. 도메인 폴더 구조 생성 및 modles 도메인 별 분리

--- src/basic/models/index.ts
+++ src/basic/models/index.ts
@@ -1,11 +1,15 @@
-// Product domain
-export type { Product, ProductWithUI, Discount } from "./product";
+// Product domain (migrated to domain/product/models)
+export type {
+  Product,
+  ProductWithUI,
+  Discount,
+} from "../domain/product/models";
 
-// Cart domain
-export type { CartItem } from "./cart";
+// Cart domain (migrated to domain/cart/models)
+export type { CartItem } from "../domain/cart/models";
 
-// Coupon domain
-export type { Coupon } from "./coupon";
+// Coupon domain (migrated to domain/coupon/models)
+export type { Coupon } from "../domain/coupon/models";
 
-// Notification domain
-export type { Notification } from "./notification";
+// Notification domain (migrated to domain/notification/models)
+export type { Notification } from "../domain/notification/models";
